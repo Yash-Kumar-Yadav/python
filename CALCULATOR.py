8266d0bb2232f6b59c5e44d1841fe61452ac7389
Yash-Kumar-Yadav <118496863+Yash-Kumar-Yadav@users.noreply.github.com>
2023-07-28 13:25:55 +0530

    Add files via upload

---
+++ CALCULATOR.py
@@ -0,0 +1,19 @@
+print('''
++ ADD
+- SUBSTRACT
+* MULTIPLY
+/ DIVIDE
+''')
+num1=int(input("Enter the value:-"))
+num2=int(input("Enter the value:-"))
+opr=input("Enter the operator..(+,-,*,/):- ")
+if opr=="+":
+    print(num1+num2)
+elif opr=="-":
+    print(num1-num2)
+elif opr=="*":
+    print(num1*num2)
+elif opr=="/":
+    print(num1/num2)
+else:
+    print("invalid operator")
