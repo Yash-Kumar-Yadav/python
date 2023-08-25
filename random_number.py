32cf38ad0e829651ea1cecaa752a02eade3b6ab0
Yash-Kumar-Yadav <118496863+Yash-Kumar-Yadav@users.noreply.github.com>
2023-08-25 08:39:23 +0530

    Random_number.py
    Guessing random number from computer and user

--- random_number.py
+++ random_number.py
@@ -1,10 +1,13 @@
 import random
 CompNumber=random.randrange(1,201)
-userInput=int(input("Input from user---"))
+userInput=int(input("Input from user:-"))
 if userInput>CompNumber:
+    print("Computer Number is  ",CompNumber)
     print("Your value is high")
 elif userInput<CompNumber:
+    print("Computer Number is  ",CompNumber)
     print("Your value is low")
 else:
     userInput=CompNumber
+    print("Computer Number is  ",CompNumber)
     print("Your value is correct")
